491789fe6160589ea06374e9b6f9ec66ba10f204
Lord Affan <124012025+affanlst@users.noreply.github.com>
2023-11-10 11:38:18 +0700

    navbar+splashscreen frame

--- app/src/main/java/com/laundryukurukur/MainActivity.kt
+++ app/src/main/java/com/laundryukurukur/MainActivity.kt
@@ -3,8 +3,10 @@ package com.laundryukurukur
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.laundryukurukur.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread.sleep(3000)
