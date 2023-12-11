0712174df23ea2b158fdd7d3db32bf0e79f0ca26
AndhikaW <andhikaw61@gmail.com>
2023-12-11 07:24:05 +0700

    no message

--- app/src/main/java/com/laundryukurukur/profile.kt
+++ app/src/main/java/com/laundryukurukur/profile.kt
@@ -1,10 +1,13 @@
 package com.laundryukurukur
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.laundryukurukur.databinding.FragmentHomeBinding
@@ -14,6 +17,7 @@ import com.laundryukurukur.databinding.FragmentProfileBinding
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
 
 class profile : Fragment() {
     // TODO: Rename and change types of parameters
@@ -30,11 +34,11 @@ class profile : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val user = firebaseAuth.currentUser
         val Email = user?.email
+        val logoutButton = view?.findViewById<Button>(R.id.button_logout)
+
         // Inflate the layout for this fragment
         val view: View =  inflater.inflate(R.layout.fragment_profile, container, false)
         var nama : TextView = view.findViewById(R.id.p_user)
@@ -45,8 +49,16 @@ class profile : Fragment() {
         if(user!=null){
             email.text = Email
         }
+
+        logoutButton?.setOnClickListener {
+            val intent = Intent(requireContext(), login::class.java)
+            startActivity(intent)
+
+            requireActivity().supportFragmentManager.popBackStack()
+        }
         return view
     }
+
 
     companion object {
         /**
