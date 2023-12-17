cfe55f6c4c61e45399d4e180074d3ca89d65ed7f
LAPTOP-T9IKDCFJ\bteja402 <bteja402@gmail.com>
2023-12-17 12:12:15 +0700

    no message

--- app/src/main/java/com/laundryukurukur/profile.kt
+++ app/src/main/java/com/laundryukurukur/profile.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.laundryukurukur.databinding.FragmentHomeBinding
@@ -21,6 +22,9 @@ class profile : Fragment() {
     private var param2: String? = null
     val firebaseAuth = FirebaseAuth.getInstance()
 
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,6 +37,7 @@ class profile : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         val user = firebaseAuth.currentUser
         val Email = user?.email
         // Inflate the layout for this fragment
@@ -46,6 +51,8 @@ class profile : Fragment() {
             email.text = Email
         }
         return view
+
+
     }
 
     companion object {
